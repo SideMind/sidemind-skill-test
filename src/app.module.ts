9f11ca48705361c575a7742e78134550bbec381a
André Ricardo Morato Rosa <65589462+dedsdead@users.noreply.github.com>
2024-02-01 20:55:12 +0100

    feat: create task resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TaskModule } from './modules/task/task.module';
 
 @Module({
-  imports: [],
+  imports: [TaskModule],
   controllers: [],
   providers: [],
 })
