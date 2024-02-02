7c236b2fd9de9da8ce18676b783a5539e9a63c6c
André Ricardo Morato Rosa <65589462+dedsdead@users.noreply.github.com>
2024-02-02 11:35:56 +0100

    feat: create task controller
    sending a task to the controller and creating it OK

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { TaskDTO } from './task.dto';
 
 @Controller('task')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  async create(@Body() data: TaskDTO) {
+    return this.taskService.create(data);
+  }
 }
